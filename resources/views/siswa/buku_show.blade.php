f68a321d97eba3dbb59094f3027205c4ebc7dcbc
Venotic <2017rifai2017@gmail.com>
2024-06-09 19:48:53 +0700

    tes titik

--- resources/views/siswa/buku_show.blade.php
+++ resources/views/siswa/buku_show.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Modernize Free</title>
+    <title>Perpustakaan</title>
     <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
     <link rel="stylesheet" href="../assets/css/styles.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
@@ -114,7 +114,7 @@
                         @elseif ($buku->is_mengantri_pengembalian())
                             <a class="btn btn-warning">Menunggu pengembalian...</a>
                         @elseif ($buku->is_mengantri_denda())
-                            <a class="btn btn-warning">Menunggu denda dibayar...</a>
+                            <a class="btn btn-warning">Menunggu denda dibayar</a>
                         @elseif ($buku->status_ketersediaan)
                             <a clasJs="btn btn-primary" href="/mengantri-peminjaman/{{ $buku->slug }}">Pinjam</a>
                         @elseif ($buku->status_ketersediaan =! false)
